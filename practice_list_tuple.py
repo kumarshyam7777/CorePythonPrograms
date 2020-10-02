43519ddee051aa16ef6d5e7f41e2a9c647599b15
kumarshyam7777 <kumar.shyam7777@gmail.com>
2020-10-02 21:57:57 +0530

    Initial commit Python Tuple Example

---
+++ practice_list_tuple.py
@@ -0,0 +1,10 @@
+fruit1 = input("Enter 1st Fruit name:")
+fruit2 = input("Enter 2nd Fruit name:")
+fruit3 = input("Enter 3rd Fruit name:")
+fruit4 = input("Enter 4th Fruit name:")
+fruit5 = input("Enter 5th fruit name:")
+fruit6 = input("Enter 6th fruit name:")
+fruit7 = input("Enter 7th fruit  name")
+
+fruit_list = [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7]
+print(fruit_list)
