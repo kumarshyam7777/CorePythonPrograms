07459e22908ecf9d36cc4b494b547e42d36e500b
kumarshyam7777 <kumar.shyam7777@gmail.com>
2020-12-10 21:35:10 +0530

    fruits tuples example

--- pr_list_tuple.py
+++ pr_list_tuple.py
@@ -6,5 +6,5 @@ f5 = input("Enter Fruit5 Name!")
 f6 = input("Enter Fruit6 Name!")
 f7 = input("Enter Fruit7 Name!")
 
-myFritList = [f1, f2, f3, f4, f5, f6, f7]
-print(myFritList)
+myFruitList = [f1, f2, f3, f4, f5, f6, f7]
+print(myFruitList)
