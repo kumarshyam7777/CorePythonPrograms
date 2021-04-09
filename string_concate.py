f91dd7967237687d765948fe181c3e98bdda7004
kumarshyam7777 <kumar.shyam7777@gmail.com>
2021-04-09 09:41:42 +0530

    Python String concatenation

--- string_concate.py
+++ string_concate.py
@@ -1,7 +1,7 @@
 # s = 'durga' + 10
 # print(s)  # String concatenation is possible with string only in Python
 
-s = 'durga' * 3
+s = 'durga' * 3 # the string is multiplied by 3 means it prints thrice times of the string.
 print(s)
 
 s = 3 * 'shyam'
