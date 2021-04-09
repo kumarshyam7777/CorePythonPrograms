8bdd5fbfe94c2d8a47033dee0781b96a50542978
kumarshyam7777 <kumar.shyam7777@gmail.com>
2021-04-09 09:42:22 +0530

    Python List Methods

--- list_method.py
+++ list_method.py
@@ -1,8 +1,7 @@
-list = [10, 20, 30, 40]
-list.append('shyam')
-print(list)
-list.remove(20)
-print(list)
-list2 = list * 2
-print(list)
+list_1 = [10, 20, 30, 40, 'shyam']
+print(list_1)
+list_1.remove(20)
+print(list_1)
+list2 = list_1 * 2
+print(list_1)
 print(list2)
